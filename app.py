12cdba3c5b41c8df3b1681884a909dfadf5df44e
philstenning <philstenning@hotmail.com>
2018-11-16 14:27:25 +0000

    chang to localhost

--- app.py
+++ app.py
@@ -25,4 +25,4 @@ with app.test_request_context():
     print(url_for('api_person'))
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='localhost', port=5000, debug=True)
